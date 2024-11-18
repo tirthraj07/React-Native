0302bbaa5503769b226725e36caa551424f9ceaf
Tirthraj Mahajan <39341897+tirthraj07@users.noreply.github.com>
2024-11-18 20:57:04 +0530

    React Native Tutorial - 18 - Custom Components

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
-import { View, Button, Alert } from "react-native";
-
+import { View } from "react-native";
+import Greet from "./components/Greet";
 
 export default function App(){
 
@@ -11,47 +11,11 @@ export default function App(){
       alignItems: 'center',
       gap:30,
       }}>
+        
+        <Greet name={"Tirthraj"} />
+        <Greet name={"Aditya"} />
+        <Greet name={"Bruce Wayne"} />
 
-      {/*  You can specify the title in the alert  */}
-      <Button 
-        title="Alert #1"  
-        onPress={()=>Alert.alert("ERROR!")}
-      />
-
-      {/*  You can also specify the message in the alert  */}
-      <Button 
-        title="Alert #2"  
-        onPress={()=>Alert.alert("ERROR!", "This is the alert message")}
-      />
-
-      {/*  You can also specify the buttons in the alert box. On Android -> MAX 3 Buttons. No restrictions on IOS  */}
-      <Button   
-        title="Alert #2"  
-        onPress={()=>Alert.alert("ERROR!", "This is the alert message", 
-          [
-            {
-              text:"Cancel",
-              onPress: ()=>{console.log("cancel pressed")}
-            },
-            {
-              text:"Okay",
-              onPress: ()=>{console.log("Okay pressed")}
-            },
-            {
-              text:"Not Okay",
-              onPress: ()=>{console.log("Not Okay pressed")}
-            }
-          ]
-        )}
-      />
-
-      {/*
-
-          There are many more APIs. Check docs: https://reactnative.dev/docs/alert
-
-      */}
-
-
-    </View>
+     </View>
   )
 }
