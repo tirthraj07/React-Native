50f2aabaf6c286da3696eef9e0db92b064626749
Tirthraj Mahajan <39341897+tirthraj07@users.noreply.github.com>
2024-11-18 23:18:47 +0530

    React Native Tutorial - 20 - StyleSheet API

--- App.js
+++ App.js
@@ -1,17 +1,48 @@
-import { View } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 
 export default function App(){
 
   return (
-    <View style={{
-      flex: 1,
-      backgroundColor: 'plum',
-      justifyContent: 'center',
-      alignItems: 'center',
-      gap:30,
-      }}>
+    <View style={style.container}>
         
+      <View style={style.box}>
+        <Text style={style.boxText}>
+          Hello World
+        </Text>
+      </View>
 
      </View>
   )
 }
+
+// You can also create a separate file and export it
+const style = StyleSheet.create({
+  container:{
+    flex: 1,
+    backgroundColor: 'plum',
+    justifyContent: 'center',
+    alignItems: 'center',
+    gap:30,
+  },
+  
+  box:{
+    width: 300,
+    height: 300,
+    flexDirection:"column",
+    justifyContent:'center',
+    alignItems:'center',
+    backgroundColor: 'pink',
+    borderColor:'black',
+    borderWidth:1,
+    borderRadius:20
+  },
+  
+  boxText:{
+    fontSize:30,
+    textDecorationLine:"underline",
+    fontWeight:"700",
+    letterSpacing:5,
+    userSelect:"text"
+  }
+
+})
