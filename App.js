86d912b866a48fdc17859d6b8d968fac35fb2409
Tirthraj Mahajan <39341897+tirthraj07@users.noreply.github.com>
2024-11-18 23:46:32 +0530

    React Native Tutorials - 21 - Multiple Styles

--- App.js
+++ App.js
@@ -5,9 +5,17 @@ export default function App(){
   return (
     <View style={style.container}>
         
-      <View style={style.box}>
+      {/*  You can combine styles by inserting them into an array.  */}
+      {/*  Note: If two styles conflict, the latter (rightmost) gets precedence  */}  
+      <View style={[style.box, style.pinkBg]}>
         <Text style={style.boxText}>
-          Hello World
+          Hello
+        </Text>
+      </View>
+      
+      <View style={[style.box, style.lightGreenBg]}>
+        <Text style={style.boxText}>
+          World
         </Text>
       </View>
 
@@ -31,10 +39,17 @@ const style = StyleSheet.create({
     flexDirection:"column",
     justifyContent:'center',
     alignItems:'center',
-    backgroundColor: 'pink',
     borderColor:'black',
     borderWidth:1,
     borderRadius:20
+  },
+
+  pinkBg: {
+    backgroundColor: 'pink'
+  },
+
+  lightGreenBg: {
+    backgroundColor: 'lightgreen'
   },
   
   boxText:{
