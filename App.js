e6c3331c4c022c62498504324253fae5f3c38dfa
Tirthraj Mahajan <39341897+tirthraj07@users.noreply.github.com>
2024-11-19 20:40:45 +0530

    React Native Tutorial - 43 - SafeAreaView

--- App.js
+++ App.js
@@ -1,65 +1,17 @@
-import { useEffect } from "react";
-import { View, Text, StyleSheet, useWindowDimensions } from "react-native";
+import { View, Text, SafeAreaView } from "react-native";
+
+// SafeAreaView can be used to display contents according to device space such as rounded corner 
+// or camera notches, sensor housing area. It adds padding from tool bar, navigation bar and tab bar.
 
 export default function App(){
-  const windowWidth = useWindowDimensions().width;
-  const windowHeight = useWindowDimensions().height;
 
-  useEffect(()=>{
-    console.log(`Window Width = ${windowWidth} AND Window Height = ${windowHeight}`)
-  }, [windowWidth, windowHeight])
-
-
-  return (
-    <View style={style.container}>
-        
-        <View style={[style.boxContainer,
-          {
-            width: windowWidth > 500 ? "30%":"60%",
-            height: windowHeight > 800 ? "30%":"50%",
-          }
-        ]}>
-          <View style={style.boxContainerBg}></View>
-          <Text style={[style.boxContainerText,
-            {
-              fontSize: windowWidth > 500 ? 50: 24,
-            }
-          ]}>
-            Hello
-          </Text>
-        </View>
-
-
-    </View>
+  return (    
+    <SafeAreaView style={{flex:1, backgroundColor: "plum"}}>
+      <View style={{flex:1}}>
+        <Text style={{textAlign:"center", fontSize:30}}>
+          Hello
+        </Text>
+      </View>
+    </SafeAreaView>
   )
 }
-
-// You can also create a separate file and export it
-const style = StyleSheet.create({
-  container:{
-    flex: 1,
-    backgroundColor: 'plum',
-    justifyContent: "center",
-    alignItems: "center"
-  },
-  boxContainer:{
-    borderWidth: 5,
-    borderColor: 'blue',
-    justifyContent: "center",
-    alignItems:"center",
-    position:"relative",
-    isolation:"isolate" 
-  },
-  boxContainerBg:{ 
-    position:"absolute",
-    width:"100%",
-    height:"100%",
-    backgroundColor:"blue",
-    opacity:0.3,
-    zIndex: -1
-  },
-  boxContainerText:{
-    textAlign:"center",
-    fontWeight: 600
-  }
-})
