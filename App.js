83299ebe805e3cce7fd7ff26ed5a6650947c2645
Tirthraj Mahajan <39341897+tirthraj07@users.noreply.github.com>
2024-11-20 21:10:00 +0530

    React Native Tutorial - 61 - Switch

--- App.js
+++ App.js
@@ -1,91 +1,52 @@
-import { View, Text, SafeAreaView, TextInput, StatusBar} from "react-native"; 
-import { useState } from "react";
+import { View, Text, SafeAreaView, TextInput, StatusBar, StyleSheet, Switch} from "react-native"; 
+import { useState, useEffect } from "react";
 
 
 export default function App(){
 
-  const [name, setName] = useState("");
-  const [story, setStory] = useState("");
+  const [isDarkMode, setIsDarkMode] = useState(false);
 
-  const [focusState, setFocusState] = useState({ name: false });
-
-  /*
-    To avoid creating separate useState hooks for each input, 
-    you can use a single state object to manage the focus state of multiple inputs. 
-    Here's a more scalable approach
-  */
-
-  const handleFocus = (inputName) => {
-    setFocusState((prev) => ({ ...prev, [inputName]: true }));
-  };
-
-  const handleBlur = (inputName) => {
-    setFocusState((prev) => ({ ...prev, [inputName]: false }));
-  };
+  const preferenceStyle = isDarkMode ? darkMode : lightMode
 
   return (    
-    <SafeAreaView style={{flex: 1, backgroundColor:"#f5f5f5", paddingTop: StatusBar.currentHeight, paddingLeft: 5}}>
 
-      <TextInput 
-        value={name}
-        onChangeText={setName}
-        placeholder="Enter Name"
+    <SafeAreaView style={[styles.container, preferenceStyle.container]}>
+      <View style={{flexDirection: "row", alignItems:"center", justifyContent: "space-around", paddingHorizontal: 5, paddingTop: 20}}>
+        <Text style={[preferenceStyle.textStyle]}>Dark Mode : </Text>
+        <Switch 
+          value={isDarkMode}
+          onValueChange={() => setIsDarkMode((previousState) => !previousState)}
+        />
 
-        keyboardAppearance="default"
-        keyboardType="default"
-        onFocus={() => handleFocus('name')}
-        onBlur={() => handleBlur('name')}
-
-        autoCapitalize="none"   // Use this for fields where there might be non-regular english like passwords emails usernames etc
-        autoCorrect={false}
-        
-        style={{
-          backgroundColor : "white",
-          fontSize: 15,
-          borderWidth: focusState.name ? 1 : 0,
-          marginBottom: 10
-        }}
-    />
-
-
-    <TextInput 
-          value={story}
-          onChangeText={setStory}
-        
-          multiline={true}
-        
-          placeholder="Enter your story"
-          keyboardAppearance="default"
-          keyboardType="default"
-          onFocus={() => handleFocus('multiLineInput')}
-          onBlur={() => handleBlur('multiLineInput')}
-
-          autoCapitalize="none"   // Use this for fields where there might be non-regular english like passwords emails usernames etc
-          autoCorrect={false}
-          
-          style={{
-            backgroundColor : "white",
-            fontSize: 15,
-            borderWidth: focusState.multiLineInput ? 1 : 0,
-            marginBottom: 10,
-            minHeight: 100,
-            textAlignVertical: "top"
-          }}
-      />
-
-    <View>
-      <Text style={{fontSize: 30}}>
-        I am {String(name).trim()===""?"...":name}
-      </Text>
-    </View>
-
-    <View style={{marginTop:20}}>
-      <Text style={{fontSize:15}}>and this is my story..</Text>
-      <Text style={{fontSize: 30}}>
-        {String(story).trim()===""?"...":story}
-      </Text>
-    </View>
+      </View>
 
     </SafeAreaView>
   )
 }
+
+const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+    paddingTop: StatusBar.currentHeight,
+    paddingLeft: 5,
+  }
+
+})
+
+const lightMode = StyleSheet.create({
+  container: {
+    backgroundColor: "f5f5f5f5"
+  },
+  textStyle:{
+    color: "black"
+  }
+})
+
+const darkMode = StyleSheet.create({
+  container: {
+    backgroundColor: "#121212"
+  },
+  textStyle:{
+    color: "white"
+  }
+})
