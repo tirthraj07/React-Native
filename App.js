2476dac2374bc29ae3e3a14015b8d8c93b803496
Tirthraj Mahajan <39341897+tirthraj07@users.noreply.github.com>
2024-11-20 18:53:09 +0530

    React Native Tutorial - 59 - TextInput Props

--- App.js
+++ App.js
@@ -4,26 +4,36 @@ import { useState } from "react";
 
 export default function App(){
 
-  const [name, setName] = useState("");
+  const [pin, setPin] = useState();
+  const [isPinFocused, setIsPinFocused] = useState(false);
 
   return (    
     <SafeAreaView style={{flex: 1, backgroundColor:"#f5f5f5", paddingTop: StatusBar.currentHeight, paddingLeft: 5}}>
 
       <TextInput 
-        value={name}
-        onChangeText={setName}
-        placeholder="Enter Name"
+        value={pin}
+        onChangeText={setPin}
+        placeholder="Enter Pin"
+        secureTextEntry         // Makes the text input hidden -> Password
+        keyboardAppearance="default"
+        keyboardType="numeric"
+        onFocus={() => setIsPinFocused(true)} // Use onFocus for when the input is focused
+        onBlur={() => setIsPinFocused(false)} // Use onBlur for when the input loses focus
+
+        autoCapitalize="none"   // Use this for fields where there might be non-regular english like passwords emails usernames etc
+        autoCorrect={false}
+
         style={{
           backgroundColor : "white",
           fontSize: 15,
-          borderWidth: 1,
+          borderWidth: isPinFocused? 1: 0,
           marginBottom: 10
         }}
     />
 
     <View>
       <Text style={{fontSize: 30}}>
-        Hello {name}
+        Hello {pin}
       </Text>
     </View>
 
