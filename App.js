37dba870139a1f71f179f4485837b523edaee356
Tirthraj Mahajan <39341897+tirthraj07@users.noreply.github.com>
2024-11-20 18:37:06 +0530

    React Native Tutorial - 58 - TextInput

--- App.js
+++ App.js
@@ -1,128 +1,32 @@
-import { View, Text, SafeAreaView, StyleSheet, Platform, SectionList} from "react-native"; 
-import pokemonList from "./grouped_data.json";
-
-/*
-      <FlatList
-        data={pokemonList}
-        renderItem={({item})=>{
-          return(
-          <View key={item.id} style={style.boxContainer}>
-            <Text style={style.pokemonName}>{item.name}</Text> 
-            <Text style={[style.pokemonType, {
-              backgroundColor: item.type.toLowerCase() == "fire"?"red":item.type.toLowerCase() == "water" ? "blue" : item.type.toLowerCase() == "grass"?"green":"yellow"
-            }]}>{item.type}</Text>
-          </View>
-          )
-        }}  
-        
-        ItemSeparatorComponent={
-          <View style={{height: 20}}>
-          </View>
-        }
-
-        ListEmptyComponent={
-          <Text style={{textAlign:"center"}}>
-            No items found
-          </Text>
-        }
-
-        ListHeaderComponent={
-          <Text style={{textAlign: "center", fontSize: 30, marginBottom: 10}}>
-            List of Pokemon
-          </Text>
-        }
-
-        ListFooterComponent={
-          <Text style={{textAlign: "center", fontSize: 30, marginTop: 10}}>
-            End of List
-          </Text>
-        }
-
-      />
-*/
+import { View, Text, SafeAreaView, TextInput, StatusBar} from "react-native"; 
+import { useState } from "react";
 
 
 export default function App(){
 
+  const [name, setName] = useState("");
 
-  // The SectionList component has two mandatory props
-  // 1. is the sections -> source of information
-  // 2. renderItem -> function that returns the JSX to render each item 
-
-  // Note that the source information must have a 'data' array that is used for rendering
-   
   return (    
-    <SafeAreaView style={{flex: 1, backgroundColor:"#f5f5f5"}}>
+    <SafeAreaView style={{flex: 1, backgroundColor:"#f5f5f5", paddingTop: StatusBar.currentHeight, paddingLeft: 5}}>
 
-      <SectionList 
-        sections={pokemonList}
-        renderItem={({item})=>{
-          // Item is each element in the data array
+      <TextInput 
+        value={name}
+        onChangeText={setName}
+        placeholder="Enter Name"
+        style={{
+          backgroundColor : "white",
+          fontSize: 15,
+          borderWidth: 1,
+          marginBottom: 10
+        }}
+    />
 
-          return (
-            <View style={style.boxContainer}>
-              <Text style={style.pokemonName}>{item}</Text> 
-            </View>
-          )
-        }}      
-
-        renderSectionHeader={({section})=>(
-          <View style={{justifyContent: 'center', flexDirection:"row"}}>
-            <Text style={style.pokemonType}>{section.type}</Text>
-          </View>
-        )}
-      
-        ItemSeparatorComponent={()=>(
-          <View style={{height: 10}} ></View>
-        )}
-
-        SectionSeparatorComponent={()=>(
-          <View style={{height: 20}}></View>
-        )}
-
-      />
+    <View>
+      <Text style={{fontSize: 30}}>
+        Hello {name}
+      </Text>
+    </View>
 
     </SafeAreaView>
   )
 }
-
-
-const style = StyleSheet.create({
-  boxContainer:{
-    borderWidth:1,
-    justifyContent: "center",
-    alignItems:"center",
-    backgroundColor:"white",
-    width:"60%",
-    margin:"auto",
-    padding: 20,
-    borderRadius: 20,
-
-    ...Platform.select({
-      "android":{
-        elevation: 10
-      },
-      "ios":{
-        shadowColor:"black",
-        shadowRadius:10,
-        shadowOpacity:0.5 
-      } 
-    })
-  },
-  pokemonName:{
-    textAlign:"center",
-    fontWeight: 700,
-    fontSize: 20
-  },
-  pokemonType: {
-    borderWidth: 2,
-    width: 100,
-    textAlign:"center",
-    fontSize: 20,
-    borderRadius:20,
-    margin:10
-  }
-
-
-
-})
